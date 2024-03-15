ac40b0520e36f6012b6256423013f07359b457fd
QuentinBrivet <brivetq@gmail.com>
2024-03-15 14:24:35 +0100

    bonjour

---
+++ src/Ola.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Ola extends Component {
+    render () {
+        return(
+            <div className="Ola">
+                <h1> Hello </h1>
+            </div>
+        )
+    }
+}
+
+export default Ola
